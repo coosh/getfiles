8b4a769e56f0b219c93910afc9c9f91566c14d9e
Dennis Theisen <dennis@jadedpixel.com>
2010-04-15 15:44:00 +0200

    Use Net:HTTP to download the source of the HTML file. (does not work atm as basic auth is disabled inside Shopify).

--- get_files.rb
+++ get_files.rb
@@ -6,22 +6,37 @@
 ## Download the "Uploaded Files" html source 
 ## Find the prefix url in the grab the url section
 ## run with
-## get_files.rb <htmsource> <prefixurl>
+## get_files.rb <sourceurl> <prefixurl> <email> <password>
 ##
 ##
 
 require 'rubygems'
 require 'hpricot'
+require 'net/http'
 
 source = ARGV[0]
 prefix = ARGV[1]
+email, password = ARGV[2], ARGV[3]
 
-f = File.open(source, "r")
-doc = Hpricot.parse(f)
-doc.search( "//div[@class='asset-image']//img" ).each do |asset|
-  url="#{prefix}/#{asset.attributes['title']}"
-  `wget #{url}`
+
+def read_remote_file(url, username = nil, password = nil)
+  uri = URI.parse(url)
+  Net::HTTP.start(uri.host, uri.port) do |http|
+    req = Net::HTTP::Get.new(uri.path)    
+    req.basic_auth(username, password) if username && password
+    response = http.request(req)
+    response.body
+  end
 end
 
-f.close
+def download_file()
+  
+end
 
+
+doc = Hpricot.parse(read_remote_file(source, email, password))
+doc.search( "//div[@class='asset-image']//img" ).each do |asset|
+  url = "#{prefix}/#{asset.attributes['title']}"
+  # TODO: use Net::HTTP or 'open-uri' to download the file so it works on windows as well
+  `wget #{url}`
+end
